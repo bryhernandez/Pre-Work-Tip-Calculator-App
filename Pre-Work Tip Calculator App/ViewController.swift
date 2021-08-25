d635aa0ffcd278bf1ea2415bff83afcc30d1715b
Bryan Hernandez <bryhernandez@csumb.edu>
2021-08-24 20:54:42 -0700

    Initial Commit

---
+++ Pre-Work Tip Calculator App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pre-Work Tip Calculator App
+//
+//  Created by Bryan Hernandez on 8/24/21.
+//  Copyright © 2021 Bryan Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pre-Work Tip Calculator AppTests/Pre_Work_Tip_Calculator_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pre_Work_Tip_Calculator_AppTests.swift
+//  Pre-Work Tip Calculator AppTests
+//
+//  Created by Bryan Hernandez on 8/24/21.
+//  Copyright © 2021 Bryan Hernandez. All rights reserved.
+//
+
+import XCTest
+@testable import Pre_Work_Tip_Calculator_App
+
+class Pre_Work_Tip_Calculator_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
